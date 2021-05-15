0d826c320dd984b3c7105c6f0ddbbe1c2b5d3d89
helmi75 <51704295+helmi75@users.noreply.github.com>
2021-05-15 23:14:55 +0200

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -299,7 +299,7 @@ def crypto_a_vendre(exchange, minutes, market ):
         df_hystoric_order[name_crypto_low]= pd.DataFrame.from_dict(exchange.fetchMyTrades(name_crypto, since = int(x)))
         st.write(df_hystoric_order[name_crypto_low])
         index_dernier_ordre = df_hystoric_order[name_crypto_low].index.max() 
-        st.write( index_dernier_ordre )
+        st.write(df_hystoric_order[name_crypto_low].loc[index_dernier_ordre])
         liste_df.append(df_hystoric_order[name_crypto_low].loc[index_dernier_ordre][['timestamp ','datetime','symbol','side','price','amount','cost']])
         
         '''
